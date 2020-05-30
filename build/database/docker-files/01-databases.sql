6bc6a70cf4de3aa7e853d14a20087717386c1c83
Alby Hernández <donfumero@gmail.com>
2020-05-30 19:06:30 +0100

    Uploading changes

--- build/database/docker-files/01-databases.sql
+++
@@ -1,7 +0,0 @@
-# Create databases
-CREATE DATABASE IF NOT EXISTS `service_test`;
-
-# Create user for the services
-CREATE USER 'normal_user'@'%' IDENTIFIED BY 'normal_password';
-GRANT ALL PRIVILEGES ON `service_%`.* TO 'normal_user'@'%';
-
